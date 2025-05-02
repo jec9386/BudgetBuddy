0be2175f0d0d0a17d5ca9289d79f815e617ebb5a
jec9386 <jerrychandev@gmail.com>
2025-05-02 01:10:53 -0400

    custom search java file added

--- src/main/java/com/pluralsight/CustomSearch.java
+++ src/main/java/com/pluralsight/CustomSearch.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class CustomSearch {
@@ -8,7 +9,7 @@ public class CustomSearch {
     public static void displayCustomSearch(ArrayList<Transaction> transactions) {
         int choice;
 
-        do{
+        do {
             String customSearchScreenPrompt =
                     "\n                 Custom Search Options                 \n" +
                             "------------------------------------------------\n" +
@@ -50,27 +51,101 @@ public class CustomSearch {
 
 //------------------METHODS-----------------------------------------------------------------------------
 
+    //custom search form a start date
+    private static void showStartDateToNow(ArrayList<Transaction> transactions) {
+        Ledger.sortTransactions(transactions); // Sort if needed
+        LocalDate startDate = console.promptForDate("Enter start date (yyyy-MM-dd): ");
+        LocalDate today = LocalDate.now();
+        boolean found = false;
 
-    private static void showStartDateToNow(ArrayList<Transaction> transactions)
+        for (Transaction t : transactions) {
+            LocalDate transactionDate = t.getDate();//set date to variable to better compare it
+            if ((transactionDate.isEqual(startDate) || transactionDate.isAfter(startDate)) &&
+                    !transactionDate.isAfter(LocalDate.now())) {//transaction is equal to the start date or transaction is after start date up to now/
+                System.out.println(t.toPretty());
+                found = true;
+            }
+        }
 
-    //
-    private static void showAllTransactionsUpToEndDate(ArrayList<Transaction> transactions)
+        if (!found) {
+            System.out.println("No transactions found from that date to now.");
+        }
+    }
 
-    //
-    private static void showByDescription(ArrayList<Transaction> transactions)
+    //custom end date not just now
+    private static void showAllTransactionsUpToEndDate(ArrayList<Transaction> transactions) {
+        Ledger.sortTransactions(transactions);
+        LocalDate endDate = console.promptForDate("Enter the end date (yyyy-MM-dd) to get transactions up to that date: ");
+        boolean found = false;
+
+        for (Transaction t : transactions) {
+            LocalDate transactionDate = t.getDate();
+            if (!transactionDate.isAfter(endDate)) {//if the date isn't after the endDate print it.
+                System.out.println(t.toPretty());
+                found = true;
+            }
+            if (!found) {
+                System.out.println("No transactions found up to the specified end date.");
+            }
+
+        }
+    }
+
+
+    //custom transaction search by description
+    private static void showByDescription(ArrayList<Transaction> transactions) {
+        Ledger.sortTransactions(transactions);
+        String description = console.promptForString("Enter description: ");
+
+        boolean found = false; //this needs to be true there be a transaction the user is looking for
+        for (Transaction t : transactions) {
+            if (t.getDescription().equalsIgnoreCase(description)) {
+                System.out.println(t.toPretty());
+                found = true;
+            }
+        }
+        if (!found) {// If no object during iteration turns found to true then print this so user knows.
+            System.out.println("No transactions found for that description.");
+        }
+    }
 
     //custom transaction search by vendor
     private static void showByVendor(ArrayList<Transaction> transactions) {
         Ledger.sortTransactions(transactions);
         String vendor = console.promptForString("Enter vendor name: ");
+
+        boolean found = false; //this needs to be true there be a transaction the user is looking for
         for (Transaction t : transactions) {
             if (t.getVendor().equalsIgnoreCase(vendor)) {
                 System.out.println(t.toPretty());
+                found = true;
             }
         }
+
+        if (!found) {// If no object during iteration turns found to true then print this so user knows.
+            System.out.println("No transactions found for that vendor.");
+        }
+
+
     }
 
-    //
-    private static void showByAmount(ArrayList<Transaction> transactions)
+    //custom transaction search by amount
+    private static void showByAmount(ArrayList<Transaction> transactions) {
+        Ledger.sortTransactions(transactions);
+        System.out.println("Enter amount to search (e.g., -100.00 for payments, 250.00 for deposits, DON'T add comma): ");
+        double amountToSearch = console.promptForDouble("Amount: $");
 
+        boolean found = false; //this needs to be true there be a transaction the user is looking for
+        for (Transaction t : transactions) {
+            if (t.getAmount() == amountToSearch) {
+                System.out.println(t.toPretty());
+                found = true;
+            }
+        }
+
+
+        if (!found) {// If no object during iteration turns found to true then print this so user knows.
+            System.out.println("No transactions found for that amount.");
+        }
+    }
 }
